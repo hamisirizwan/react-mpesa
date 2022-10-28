4380e9e3e0fc722dcbc374ecfe22916f949e7c6d
hamisirizwan <hamisirizwan5@gmail.com>
2022-10-28 05:25:16 -0700

    checking cors errors

--- src/pages/PayPage.js
+++ src/pages/PayPage.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function PayPage() {
   const [details, setDetails] = useState({
@@ -6,19 +9,80 @@ function PayPage() {
     amount: "",
   });
 
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+    // console.log(details);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //validate inputs
+    if (!details.amount || !details.phone) {
+      return alert("enter all fields");
+    }
+    if (details.amount < 1) {
+      return alert("enter valid amount");
+    }
+    if (details.phone.length !== 10) {
+      return alert("invalid phone number");
+    }
+    if (
+      details.phone.substring(0, 2) !== "07" &&
+      details.phone.substring(0, 2) !== "01"
+    ) {
+      return alert("valid number should start with 07 or 01");
+    }
+    //send request
+
+    try {
+      const key = "s0L1YFYzr4eGANAiVtmhPum9Us9pQnaT";
+      const secret = "goca51EHNnZnQHOA";
+      const auth = new Buffer.from(`${key}:${secret}`).toString("base64");
+
+      const tokenData = await axios.get(
+        "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Basic ${auth}`,
+          },
+        }
+      );
+
+      console.log(tokenData);
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
   return (
     <div className="card p-4 mb-3">
       <p className="text fs-4 fw-normal text-center font-monospace">
         Give It A Try
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label className="form-label">Phone:</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            name="phone"
+            value={details.phone}
+            required
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-3">
           <label className="form-label">Amount:</label>
-          <input type="number" className="form-control" />
+          <input
+            type="number"
+            className="form-control"
+            name="amount"
+            value={details.amount}
+            required
+            onChange={handleChange}
+          />
         </div>
 
         <button type="submit" className="btn btn-block btn-primary col-12">
